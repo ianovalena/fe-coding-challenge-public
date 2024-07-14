5450889835656853e4cea250b33df717f9a38cdc
alena <alena.ianova.it@gmail.com>
2024-07-14 15:32:43 +0300

    Revert "generated basic routing, module and components"
    This reverts commit d152f45bda12eef36a22bbe899da46192cf2d6d0.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import {ProductsModule} from "./modules/products/products.module";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    CommonModule,
-    AppRoutingModule,
-    ProductsModule,
-  ],
+  imports: [BrowserModule, CommonModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
