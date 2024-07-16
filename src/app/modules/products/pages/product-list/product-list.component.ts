2fd2b228916104701f99707bce48de2724883313
alena <alena.ianova.it@gmail.com>
2024-07-16 15:21:40 +0300

    added small error message

--- src/app/modules/products/pages/product-list/product-list.component.ts
+++ src/app/modules/products/pages/product-list/product-list.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component, OnInit, signal, WritableSignal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, OnInit, signal, WritableSignal } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { catchError, concat, of, Subject, switchMap, tap } from 'rxjs';
 import { Product } from '../../interfaces/product';
 import { CommonModule } from '@angular/common';
-import {ProductCardComponent} from '../../components/product-card/product-card.component';
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-product-list',
@@ -20,16 +21,21 @@ export class ProductListComponent implements OnInit {
   isLoading: WritableSignal<boolean> = signal(true);
   products: WritableSignal<Product[]> = signal([]);
   displayShowMoreButton: WritableSignal<boolean> = signal(true);
+  displayLoadingError: WritableSignal<boolean> = signal(false);
 
   private loadMore: Subject<void> = new Subject<void>();
   private page = 0;
 
-  constructor(private readonly productService: ProductService) {}
+  constructor(
+    private readonly productService: ProductService,
+    private readonly destroyRef: DestroyRef,
+  ) {}
 
   ngOnInit() {
     this.loadMore.asObservable()
       .pipe(
         tap(() => this.isLoading.set(true)),
+        tap(() => this.displayLoadingError.set(false)),
         switchMap(() => this.productService.get(this.page)),
         //retry(1),
         tap(result => {
@@ -38,10 +44,11 @@ export class ProductListComponent implements OnInit {
         }),
         tap(() => this.page++),
         catchError((err, caught) => {
-          console.log('error popup!');
+          this.displayLoadingError.set(true);
           return concat(of(null), caught);
         }),
         tap(() => this.isLoading.set(false)),
+        takeUntilDestroyed(this.destroyRef),
       )
       .subscribe();
 
